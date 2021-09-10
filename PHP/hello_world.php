12d4d9b374cf96147de0de0faf8a2fce3e2c936f
6210513061 <6210513061@psu.ac.th>
2021-09-11 02:34:41 +0700

    quiz-2

---
+++ PHP/hello_world.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World in PHP</title>
+</head>
+<body>
+    <?php 
+    // test heelo world
+    #course id:477-302
+    /*semeter: 1-2564 */
+    
+        /*echo "<div style='color:read'><h1>Hello World in PHP</h1></div> with HTML!";*/
+        $text1 = "I am";
+        $age = 40;
+        $text2 = "year old";
+        echo $text;
+        echo $echo;
+        echo $text2;
+        echo $text1 . $age . $text2;
+    ?>
+
+    <div style="color:green">
+        <h1>
+            <?php echo "Hello world in PHP"; ?>
+        </h1>
+    </div>
+</body>
+</html>
---
+++ quiz 2/dialog_password.js
@@ -0,0 +1,4 @@
+function dialog() {
+    var x=document.getElementById("dialog");
+    x.open=true
+}
